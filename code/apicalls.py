3019bed35a14ad01e71f73ec184555224222ea75
zoeymccarthy <zpmccart@syr.edu>
2025-04-14 13:42:41 -0400

    wrote the apicalls.py functions

--- code/apicalls.py
+++ code/apicalls.py
@@ -1,19 +1,39 @@
 import requests
 
 # Put your CENT Ischool IoT Portal API KEY here.
-APIKEY = "APIKEYHERE"
+APIKEY = "3a9cfc917c415be7c1c4116d"
 
 def get_google_place_details(google_place_id: str) -> dict:
-    pass # Implement this function
-    
+    url = "https://cent.ischool-iot.net/api/google/place/details"
+    querystring = {google_place_id: google_place_id}
+    headers = {'X-API-KEY': APIKEY}
+    response = requests.get(url, headers= headers, params = querystring)
+    response.raise_for_status() 
+    return response.json()  # Return the JSON response as a dictionary
 def get_azure_sentiment(text: str) -> dict:
-    pass # Implement this function
+    url = "https://cent.ischool-iot.net/api/azure/sentiment"
+    querystring  = {"text": text}
+    headers = {"X-API-KEY": APIKEY}
+    response = requests.get(url, headers=headers, params = querystring)
+    response.raise_for_status()
+    return response.json()  # Return the JSON response as a dictionary
 
 def get_azure_key_phrase_extraction(text: str) -> dict:
-    pass # Implement this function
+    url = "https://cent.ischool-iot.net/api/azure/sentiment"
+    querystring  = {"text": text}
+    headers = {"X-API-KEY": APIKEY}
+    response = requests.get(url, headers=headers, params = querystring)
+    response.raise_for_status()
+    return response.json()
+
 
 def get_azure_named_entity_recognition(text: str) -> dict:
-    pass # Implement this function
+    url = "https://cent.ischool-iot.net/api/azure/ner"
+    querystring  = {"text": text}
+    headers = {"X-API-KEY": APIKEY}
+    response = requests.get(url, headers=headers, params = querystring)
+    response.raise_for_status()
+    return response.json()
 
 
 def geocode(place:str) -> dict:
