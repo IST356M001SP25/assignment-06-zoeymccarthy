fd606e798427df652b2396bb3a54c0976965ee65
zoeymccarthy <zpmccart@syr.edu>
2025-05-03 16:06:02 -0400

    wrote the sentiment_step function

--- code/assignment_etl.py
+++ code/assignment_etl.py
@@ -41,14 +41,44 @@ def sentiment_step(reviews: str|pd.DataFrame) -> pd.DataFrame:
     '''
       2. reviews --> sentiment_step --> review_sentiment_by_sentence
     '''
-    pass # TODO: implement this function
+    if isinstance(reviews, str):
+        reviews_df = pd.read_csv(reviews)
+    else:
+        reviews_df = reviews
+    #1. For each place in the input, call the azure sentiment API to get the sentiment of the text.
+      #Extract the results under ['results']['documents'][0]. This should be a dict.
+      #Add to the results the place_id and name of the place, author_name, and rating 
+    sentiments = []
+    for index, row in reviews_df.iterrows():
+        sentiment = get_azure_sentiment(row['text'])
+        sentiment_item = sentiment['results']['documents'][0]
+        sentiment_item['place_id'] = row['place_id']
+        sentiment_item['name'] = row['name']
+        sentiment_item['author_name'] = row['author_name']
+        sentiment_item['rating'] = row['rating']
+        sentiments.append(sentiment_item)
+    #2. Use json_normalize to Transform the json to be at the sentences level, adding back the place_id, name, author_name, and rating from the parent level.
+    # construct dataframe at the sentence level, include place_id, name from parent level
+    sentiment_df = pd.json_normalize(sentiments, record_path="sentences", meta=["place_id", 'name', 'author_name', 'rating'])
+    
+    #3. Rename the "text" column to "sentence_text" and the "sentiment" column to "sentence_sentiment"
+    sentiment_df.rename(columns={'text': 'sentence_text'}, inplace=True)
+    sentiment_df.rename(columns={'sentiment': 'sentence_sentiment'}, inplace=True)
+
+    #4. Filter to these columns: place_id, name, author_name, rating, sentence_text, sentence_sentiment, confidenceScores.positive,confidenceScores.neutral, confidenceScores.negative
+    sentiment_df = sentiment_df[['place_id', 'name', 'author_name', 'rating', 'sentence_text', 'sentence_sentiment', 'confidenceScores.positive', 'confidenceScores.neutral', 'confidenceScores.negative']]
+
+    # save sentiment_df to cache, return sentiments_df
+    sentiment_df.to_csv(CACHE_SENTIMENT_FILE, index=False, header=True)
+    return sentiment_df
+    
 
 
 def entity_extraction_step(sentiment: str|pd.DataFrame) -> pd.DataFrame:
     '''
       3. review_sentiment_by_sentence --> entity_extraction_step --> review_sentiment_entities_by_sentence
     '''
-    pass # TODO: implement this function
+    
 
 
 if __name__ == '__main__':
